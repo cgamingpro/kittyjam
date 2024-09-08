ad5f6a27a9019f53ec4d0ef28ffc3394cb334e41
calikoberen01 <149206255+calikoberen01@users.noreply.github.com>
2024-09-08 19:18:06 +0300

    playerMovementJump
    i added space button can jump

--- Assets/Scripts/Movmwent/PlayerController.cs
+++ Assets/Scripts/Movmwent/PlayerController.cs
@@ -8,7 +8,14 @@ public class PlayerController : MonoBehaviour
     public GameObject camera;
     CharacterController characterController;
     [SerializeField] float speed;
-
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float maxjumpHeight = 1.5f;
+    
+    private Vector3 verticalvelocity;
+    private bool isGrounded;
+    public Transform groundCheckpoint;
+    public float groundCheckpointRadius = 0.4f;
+    public LayerMask groundMask;       // What is considered ground
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -17,11 +24,28 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        float inputx = Input.GetAxisRaw("Horizontal");
-        float inputz = Input.GetAxisRaw("Vertical");
+        isGrounded = Physics.CheckSphere(groundCheckpoint.position, groundCheckpointRadius, groundMask);
 
+        if (isGrounded && verticalvelocity.y < 0)
+        {
+           verticalvelocity.y = -2f; // Reset the velocity when grounded
+        }
+
+        float inputx = Input.GetAxis("Horizontal");
+        float inputz = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            verticalvelocity.y = Mathf.Sqrt(maxjumpHeight * -2f * gravity);
+        }
+       
+        verticalvelocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(verticalvelocity * Time.deltaTime);
+        
         Move1(inputx,inputz);
     }
+
 
     public void Move1(float inpx,float inpz)
     {
@@ -29,4 +53,6 @@ public class PlayerController : MonoBehaviour
         Vector3 move = camera.transform.right * inpx + camera.transform.forward * inpz;
         characterController.Move(move * speed * Time.deltaTime);
     }
+    
+
 }
