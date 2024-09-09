ab5b5d0a21af620c744f66ab4b6c6f054fcae2c7
calikoberen01 <149206255+calikoberen01@users.noreply.github.com>
2024-09-10 01:37:59 +0300

    i tried to make wall jump

--- Assets/Scripts/Movmwent/PlayerController.cs
+++ Assets/Scripts/Movmwent/PlayerController.cs
@@ -10,12 +10,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float gravity = -9.81f;
     [SerializeField] float maxjumpHeight = 1.5f;
-    
+    public float maxWalljumpHeight = 5f;
+    public float wallJumpForce = 8f;
+
     private Vector3 verticalvelocity;
+    private bool isTouchingWall;
     private bool isGrounded;
+    private bool hasJumped = false;
+   
+    private bool canWallJump = true;  
+    [SerializeField] float wallJumpCooldown = 2f;  
+    private float wallJumpTimer = 0f;
+
+    public Transform wallCheck;
     public Transform groundCheckpoint;
+    public float wallDistance = 0.5f;
     public float groundCheckpointRadius = 0.4f;
     public LayerMask groundMask;       // What is considered ground
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -24,35 +36,81 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
+        isTouchingWall = Physics.CheckSphere(wallCheck.position, wallDistance, groundMask);
         isGrounded = Physics.CheckSphere(groundCheckpoint.position, groundCheckpointRadius, groundMask);
 
         if (isGrounded && verticalvelocity.y < 0)
         {
            verticalvelocity.y = -2f; // Reset the velocity when grounded
+            hasJumped = false;
+            canWallJump = true;
+            wallJumpTimer = 0f;
+        }
+
+        if (!canWallJump)
+        {
+            wallJumpTimer += Time.deltaTime;
+            if (wallJumpTimer >= wallJumpCooldown)
+            {
+                canWallJump = true; // Cooldown bittiðinde tekrar wall jump yapabilir
+                wallJumpTimer = 0f;
+            }
         }
 
         float inputx = Input.GetAxis("Horizontal");
         float inputz = Input.GetAxis("Vertical");
 
+        
+        
+           
+        
+            
+        Vector3 move = camera.transform.right * inputx + camera.transform.forward * inputz;
+
+        move.y = 0;
+
+        characterController.Move(move * speed * Time.deltaTime);
+        
+
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             verticalvelocity.y = Mathf.Sqrt(maxjumpHeight * -2f * gravity);
+            
         }
-       
+
+        if (Input.GetButtonDown("Jump") && isTouchingWall && !isGrounded && !hasJumped && canWallJump)
+        {
+            
+            verticalvelocity.y = Mathf.Sqrt(maxWalljumpHeight * -2f * gravity);
+
+            Vector3 wallJumpDirection = -camera.transform.forward;
+            characterController.Move(wallJumpDirection * wallJumpForce * Time.deltaTime);
+            
+            hasJumped = true;
+            canWallJump = false;
+            wallJumpTimer = 0;
+        }
+
+        
+
         verticalvelocity.y += gravity * Time.deltaTime;
 
         characterController.Move(verticalvelocity * Time.deltaTime);
         
-        Move1(inputx,inputz);
     }
 
 
-    public void Move1(float inpx,float inpz)
-    {
-        Vector3 velocity = new Vector3(inpx,0, inpz);
-        Vector3 move = camera.transform.right * inpx + camera.transform.forward * inpz;
-        characterController.Move(move * speed * Time.deltaTime);
-    }
     
 
+
+
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (characterController.collisionFlags == CollisionFlags.Sides)
+        {
+            Debug.DrawRay(hit.point, hit.normal, Color.red, 2f);
+        }
+    }
 }
