d34789643178df32b4e444aad0e5e20fd5bd917b
calikoberen01 <149206255+calikoberen01@users.noreply.github.com>
2024-09-12 00:16:25 +0300

    nawmesh enemypatrollingai System

--- Assets/Scripts/enemy/EnemyPatrolingAI.cs
+++ Assets/Scripts/enemy/EnemyPatrolingAI.cs
@@ -1,26 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyPatrolingAI : MonoBehaviour
 {
+    NavMeshAgent agent;
     public Transform[] patrolPoints;
     public int targetPoint;
     public float speed;
 
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         targetPoint = 0;
     }
 
  
     void Update()
     {
-        if(transform.position == patrolPoints[targetPoint].position)
+        float distance = Vector3.Distance(transform.position, patrolPoints[targetPoint].position);
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             IncreaseTargetInt();
         }
-        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+        if(distance < 0.5f)
+        {
+            IncreaseTargetInt();
+        }
+        //transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+        agent.destination = patrolPoints[targetPoint].position;
     }
 
     void IncreaseTargetInt()
@@ -39,25 +51,50 @@ public class EnemyPatrolingAI : MonoBehaviour
     {
         for (int i = 0; i < patrolPoints.Length; i++)
         {
-            if (i >= patrolPoints.Length) 
-            {
-                Gizmos.DrawLine(patrolPoints[i].position, patrolPoints[0].position);
-                
-                i = 0;
-
-
-
-            }
-            else {
-                
-                Gizmos.DrawLine(patrolPoints[i].position, patrolPoints[i+1].position);
-            
-            
-            }
-
-            Gizmos.DrawWireSphere(patrolPoints[i+1].position, 0.4f);
-            
+            int j = GetNextIndex(i);
+            Gizmos.DrawWireSphere(GetWaypointPos(i), 0.4f);
+            Gizmos.DrawLine(GetWaypointPos(i), GetWaypointPos(j));
         }
+        
+         int GetNextIndex(int i)
+         {
+            if (i+1 == patrolPoints.Length)
+            {
+
+                return 0;
+
+            } return i + 1;
+         }   
+
+        Vector3 GetWaypointPos(int i)
+        {
+
+            return patrolPoints[i].position;
+
+        }
+
+            
+            
+
+
         
     }
 }
+//  if (i == patrolPoints.Length) 
+//  {
+//      Gizmos.DrawLine(patrolPoints[i].position, patrolPoints[0].position);
+//      
+//      i = 0;
+//
+//
+//
+//  }
+//  else {
+//      
+//      Gizmos.DrawLine(patrolPoints[i].position, patrolPoints[i+1].position);
+//  
+//  
+//  }
+//
+//  Gizmos.DrawWireSphere(patrolPoints[i+1].position, 0.4f);
+//  
